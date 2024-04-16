fcbcb54110fa567c58b7334038db696ed81dff7d
alefernandes004 <alessandramf456@gmail.com>
2024-04-16 09:09:36 -0300

    Implementação parte da validação e inclusão de tarefas

--- script/script.js
+++ script/script.js
@@ -2,44 +2,52 @@
 document.querySelector("#botao-cadastrar").addEventListener("click", () => {
    
     const form = document.querySelector("form");
-
-   
+    
     const tarefa = {
-        titulo: form.querySelector("#titulo").value,
-        descricao: form.querySelector("#descricao").value,
-    };
+        titulo : form.titulo.value,
+        descricao : form.descricao.value
+    }
 
     
-    validar(tarefa);
-    console.log(titulo, descricao)
+    if(validar(tarefa)){
+        console.log(tarefa)
+        let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+        tarefas.push(tarefa)
+        localStorage.setItem("tarefas", JSON.stringify(tarefas))
+        
+        window.location = "/"
+    }
 });
 
 
 function validar(tarefa) {
-    
+    let valid = true 
+
     limparErros();
 
     
     if (tarefa.titulo === "" || tarefa.titulo.length < 3) {
-        
-        document.querySelector("#titulo").classList.add("is-error");
-
-        document.querySelector("#titulo-error").innerText = "O título deve ter mais que 3 caracteres!";
+        document.querySelector("#titulo").classList.add("is-error")
+        document.querySelector("#titulo-error").innerText = "O título deve ter mais que 3 caracteres!"
+        valid = false;
     }
 
-   
-    if (tarefa.descricao === "" || tarefa.descricao.length < 3) {
-    
-        document.querySelector("#descricao").classList.add("is-error");
-
-        alert("A descrição deve ter mais que 3 caracteres!");
+    if(tarefa.descricao === ""){
+        document.querySelector("#descricao").classList.add("is-error")
+        document.querySelector("#descricao-error").innerText= "A descrição deve ter mais que 3 caracteres!"
+        valid = false;
     }
+
+    return valid
 }
 
 
 function limparErros() {
-   
-    document.querySelector("#titulo").classList.remove("is-error");
-    document.querySelector("#descricao").classList.remove("is-error");
-    document.querySelector("#titulo-error").innerText = "";
+    document.
+    querySelectorAll(".nes-input.is-error, nes-textarea.is-error")
+    .forEach(campo => campo.classList.remove("is-error"))
+
+    document
+    .querySelectorAll("span.is-error")
+    .forEach(span => span.innerText = "")
 }
